cf69db41ce21b01aa3b91bcaeab032246e4861c9
ATHT138 <anhthtservice@gmail.com>
2024-05-16 13:59:12 +0700

    Create project

---
+++ src/lib/theme.ts
@@ -0,0 +1,7 @@
+// import resolveConfig from "tailwindcss/resolveConfig";
+
+// import tailwindConfig from "@/tailwindConfig/tailwind.config.js";
+
+// const { theme } = resolveConfig(tailwindConfig);
+
+// export default theme;
